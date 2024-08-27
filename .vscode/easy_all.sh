d808cba8b449aaf53df4349ab8516d9cb5490b00
Michael Carlstrom <rmc@carlstrom.com>
2024-08-27 10:04:53 -0400

    cleanup
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- .vscode/easy_all.sh
+++ .vscode/easy_all.sh
@@ -3,7 +3,7 @@
 PYTHONWARNINGS=ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources; export PYTHONWARNINGS
 
 # Install any missing dependencies
-source "$(pwd)/.vscode/install_dependencies.sh" iron
+source "$(pwd)/.vscode/install_dependencies.sh"
 
 colcon build --symlink-install
 source install/setup.bash
