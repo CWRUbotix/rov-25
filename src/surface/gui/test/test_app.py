9f12de6bd13e3b2cc21e124b0f2c1731f6315388
Michael Carlstrom <rmc@carlstrom.com>
2024-09-05 11:33:46 -0400

    drop gui testing

--- src/surface/gui/test/test_app.py
+++
@@ -1,9 +0,0 @@
-from gui.app import App
-from pytestqt.qtbot import QtBot
-
-
-def test_app_instantiation(qtbot: QtBot) -> None:
-    """Unit test for App instantiation."""
-    app = App('test')
-    app.show()
-    qtbot.addWidget(app)
