0a332dc393320d3bdf4cd40a6ed152ae365ac6bb
Michael Carlstrom <rmc@carlstrom.com>
2024-09-16 18:27:18 -0400

    Delete src/surface/flight_control/test/test_keyboard_control.py

--- src/surface/flight_control/test/test_keyboard_control.py
+++
@@ -1,9 +0,0 @@
-import rclpy
-from flight_control.keyboard_control_node import KeyboardListenerNode
-
-
-def test_keyboard_listener_instantiation() -> None:
-    """Unit test for KeyboardListenerNode instantiation."""
-    rclpy.init()
-    KeyboardListenerNode()
-    rclpy.shutdown()
