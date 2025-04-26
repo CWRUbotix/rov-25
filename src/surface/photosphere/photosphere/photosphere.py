1d2e8a411a61c0f1df8f5717e49b122dfcf09053
Shannon Griswold <shannon.v.griswold@gmail.com>
2025-04-26 14:58:58 -0400

    Fixed variable names and imports

--- src/surface/photosphere/photosphere/photosphere.py
+++ src/surface/photosphere/photosphere/photosphere.py
@@ -2,14 +2,19 @@ from rclpy.node import Node
 
 from sensor_msgs.msg import Image
 from rclpy.qos import qos_profile_default
+from builtin_interfaces.msg import Time
 import pickle
 import socket
 import struct
+import rclpy
+from rclpy.executors import MultiThreadedExecutor
 
 import cv2
 
 HOST_IP = '127.0.1.1' # the server ip address
 PORT = 9997
+
+Matlike = NDArray[generic]
 
 class Photosphere(Node):
     def __init__(self) -> None:
@@ -19,26 +24,27 @@ class Photosphere(Node):
             Image, 'fisheye1_image', qos_profile_default
         )
 
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((host_ip, port))
-        data = b''
-        payload_size = struct.calcsize('Q')
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((HOST_IP, PORT))
+        self.data = b''
+        self.payload_size = struct.calcsize('Q')
+        self.bridge = CvBridge()
 
     def get_frame(self) -> None:
 
-        while len(data) < payload_size:
-            packet = client_socket.recv(4 * 1024) # 4K
+        while len(self.data) < self.payload_size:
+            packet = self.client_socket.recv(4 * 1024) # 4K
             if not packet:
                 break
-            data += packet
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
+            self.data += packet
+        packed_msg_size = self.data[:self.payload_size]
+        self.data = self.data[self.payload_size:]
         msg_size = struct.unpack('Q', packed_msg_size)[0]
 
-        while len(data) < msg_size:
-            data += client_socket.recv(4 * 1024)
-        frame_data = data[:msg_size]
-        data  = data[msg_size:]
+        while len(self.data) < msg_size:
+            self.data += self.client_socket.recv(4 * 1024)
+        frame_data = self.data[:msg_size]
+        self.data  = self.data[msg_size:]
         frame = pickle.loads(frame_data)
 
         time_msg = self.get_clock().now().to_msg()
@@ -68,7 +74,7 @@ class Photosphere(Node):
         return img_msg
 
     def shutdown(self) -> None:
-        client_socket.close()
+        self.client_socket.close()
         
 
 
