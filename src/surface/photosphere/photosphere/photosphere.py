dfcd723df05a466dcf45306c9c15d72d4c5a03b3
NoahMollerstuen <noah@mollerstuen.com>
2025-06-09 00:39:01 -0400

    Use package relative path

--- src/surface/photosphere/photosphere/photosphere.py
+++ src/surface/photosphere/photosphere/photosphere.py
@@ -1,21 +1,20 @@
-import pickle
-import socket
-import struct
-
+import os
 import cv2
 import numpy as np
-import rclpy
-from cv_bridge import CvBridge
 from numpy import generic
 from numpy.typing import NDArray
+
+import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_default
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from ament_index_python.packages import get_package_share_directory
 
 from photosphere.fisheye_projection import convert_with_matrix
 from rov_msgs.srv import GeneratePhotosphere
 
-PROJECTION_PATH = "src/surface/photosphere/images"  # relative the rov-25 repo
+PROJECTION_PATH = "src/photosphere/display/projection.jpg"  # relative the rov-25 repo
 
 
 Matlike = NDArray[generic]
@@ -24,11 +23,11 @@ class Photosphere(Node):
     def __init__(self) -> None:
         super().__init__('photosphere')
 
-        cam_1_subscriber = self.create_subscription(
+        self.cam_1_subscriber = self.create_subscription(
             Image, 'image_1', (lambda msg: self.handle_frame_msg(msg, 0)), qos_profile_default
         )
 
-        cam_2_subscriber = self.create_subscription(
+        self.cam_2_subscriber = self.create_subscription(
             Image, 'image_2', (lambda msg: self.handle_frame_msg(msg, 1)), qos_profile_default
         )
 
@@ -77,7 +76,7 @@ class Photosphere(Node):
 
         projection = convert_with_matrix(self.fisheye_frames[0], self.fisheye_frames[1])
         self.get_logger().info("Projection created")
-        cv2.imwrite('src/photosphere/display/projection.jpg', projection)
+        cv2.imwrite(os.path.join(get_package_share_directory("photosphere").split("rov-25")[0], "rov-25", PROJECTION_PATH), projection)
         self.get_logger().info("Projection saved")
 
         # fisheye_image1 = cv2.imread('src/surface/photosphere/photosphere/frame1.png')
